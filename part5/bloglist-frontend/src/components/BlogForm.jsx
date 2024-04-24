0fe1b95576a3e42f3e51ba3a47ba6759fdf7fe29
João Vitor <j.soares@aparkado.de>
2024-04-24 16:30:49 -0300

    add proptypes to blog form

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const BlogForm = ({ createBlog }) => {
   const [title, setTitle] = useState('');
@@ -51,5 +52,7 @@ const BlogForm = ({ createBlog }) => {
     </div>
   );
 };
+
+BlogForm.propTypes = { createBlog: PropTypes.func.isRequired };
 
 export default BlogForm;
