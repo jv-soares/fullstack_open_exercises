1bef25e5b1b834f9fbf9da2bebd06ff8f1a71919
João Vitor <j.soares@aparkado.de>
2024-04-27 14:59:11 -0300

    ensure likes and url appear when button is pressed

--- part5/bloglist-frontend/test/components/Blog.test.jsx
+++ part5/bloglist-frontend/test/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from '../../src/components/Blog';
 
 test('should render only title and author', () => {
@@ -20,4 +21,25 @@ test('should render only title and author', () => {
   expect(author).toBeDefined();
   expect(likes).toBeNull();
   expect(url).toBeNull();
+});
+
+test('should also render likes and url when "view" button is pressed', async () => {
+  const blog = {
+    title: 'test blog',
+    author: 'test author',
+    likes: 1,
+    user: { name: 'test name' },
+  };
+
+  const { container } = render(<Blog blog={blog}></Blog>);
+
+  const user = userEvent.setup();
+  const viewButton = screen.getByText('view');
+  await user.click(viewButton);
+
+  const likes = container.querySelector('.blog-likes');
+  const url = container.querySelector('.blog-url');
+
+  expect(likes).not.toBeNull();
+  expect(url).not.toBeNull();
 });
