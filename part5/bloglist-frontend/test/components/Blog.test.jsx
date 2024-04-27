6d2da8990f143b55371e5d2dc6b1dd5e6f358f71
João Vitor <j.soares@aparkado.de>
2024-04-27 15:07:43 -0300

    ensure callback for like button is called

--- part5/bloglist-frontend/test/components/Blog.test.jsx
+++ part5/bloglist-frontend/test/components/Blog.test.jsx
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from '../../src/components/Blog';
 
-test('should render only title and author', () => {
-  const blog = {
-    title: 'test blog',
-    author: 'test author',
-    likes: 1,
-    user: { name: 'test name' },
-  };
+const blog = {
+  title: 'test blog',
+  author: 'test author',
+  likes: 1,
+  user: { name: 'test name' },
+};
 
+test('should render only title and author', () => {
   const { container } = render(<Blog blog={blog}></Blog>);
 
   const title = screen.getByText(blog.title, { exact: false });
@@ -24,13 +24,6 @@ test('should render only title and author', () => {
 });
 
 test('should also render likes and url when "view" button is pressed', async () => {
-  const blog = {
-    title: 'test blog',
-    author: 'test author',
-    likes: 1,
-    user: { name: 'test name' },
-  };
-
   const { container } = render(<Blog blog={blog}></Blog>);
 
   const user = userEvent.setup();
@@ -42,4 +35,24 @@ test('should also render likes and url when "view" button is pressed', async () 
 
   expect(likes).not.toBeNull();
   expect(url).not.toBeNull();
+});
+
+test('should call handleLike callback when like button is pressed', async () => {
+  const mockHandleLike = vi.fn();
+
+  const { container } = render(
+    <Blog blog={blog} handleLike={mockHandleLike}></Blog>
+  );
+
+  const user = userEvent.setup();
+  const viewButton = screen.getByText('view');
+  await user.click(viewButton);
+
+  const likeButton = container
+    .querySelector('.blog-likes')
+    .querySelector('button');
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(mockHandleLike).toHaveBeenCalledTimes(2);
 });
