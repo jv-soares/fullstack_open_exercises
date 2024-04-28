2f81c1db2baaaec8668f593b8501dd906b573352
João Vitor <j.soares@aparkado.de>
2024-04-28 20:42:56 -0300

    ensure login succeeds and fails

--- part5/e2e/tests/blog.spec.js
+++ part5/e2e/tests/blog.spec.js
@@ -1,7 +1,11 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: { username: 'testuser', name: 'Test User', password: 'test123' },
+    });
     await page.goto('/');
   });
 
@@ -10,5 +14,23 @@ describe('Blog app', () => {
     await expect(page.getByTestId('username')).toBeVisible();
     await expect(page.getByTestId('password')).toBeVisible();
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      const username = 'testuser';
+      await page.getByTestId('username').fill(username);
+      await page.getByTestId('password').fill('test123');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText(`logged in as ${username}`)).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('testuser');
+      await page.getByTestId('password').fill('test');
+      await page.getByRole('button', { name: 'login' }).click();
+      const errorLocator = page.getByText(`invalid username or password`);
+      await expect(errorLocator).toBeVisible();
+    });
   });
 });
