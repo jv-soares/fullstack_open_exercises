d5299595bc34d038a55d3c8365c51c855fd547f8
João Vitor <j.soares@aparkado.de>
2024-08-07 23:26:36 +0200

    implementing voting using react query

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,11 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
-import { getAnecdotes } from './requests';
+import { getAnecdotes, updateAnecdote } from './requests';
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const anecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => queryClient.invalidateQueries('anecdotes'),
+  });
+
   const handleVote = (anecdote) => {
-    console.log('vote');
+    anecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   const result = useQuery({
