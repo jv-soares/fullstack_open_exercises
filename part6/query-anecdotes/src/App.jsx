7200f2f7c954ffa115dd1f893c26b3a90fff1032
João Vitor <j.soares@aparkado.de>
2024-08-07 22:56:13 +0200

    fetch anecdotes using react query

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
+import { getAnecdotes } from './requests';
 
 const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote');
   };
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ];
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    retry: false,
+    queryFn: getAnecdotes,
+  });
+
+  if (result.isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  const anecdotes = result.data;
 
   return (
     <div>
