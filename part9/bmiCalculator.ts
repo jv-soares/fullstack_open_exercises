194b40a8779126db340b87a7a8e9878ba6ac082e
João Vitor <j.soares@aparkado.de>
2024-12-24 12:05:26 -0300

    validate arguments for bmi calculator

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,6 +1,11 @@
-const calculateBmi = (heightInCm: number, weightInKg: number): string => {
-  const heightInMeter = heightInCm / 100;
-  const bmi = weightInKg / (heightInMeter * heightInMeter);
+interface BmiParams {
+  heightInCm: number;
+  weightInKg: number;
+}
+
+const calculateBmi = (params: BmiParams): string => {
+  const heightInMeter = params.heightInCm / 100;
+  const bmi = params.weightInKg / (heightInMeter * heightInMeter);
 
   if (bmi < 18.5) {
     return 'Underweight';
@@ -17,4 +22,38 @@ const calculateBmi = (heightInCm: number, weightInKg: number): string => {
   }
 };
 
-console.log(calculateBmi(170, 72));
+const parseArguments = (args: string[]): BmiParams => {
+  const relevantArgs = args.slice(2);
+
+  if (relevantArgs.length < 2) throw Error('not enough arguments');
+  if (relevantArgs.length > 2) throw Error('too many arguments');
+
+  if (!isNumber(relevantArgs[0]) || !isNumber(relevantArgs[1])) {
+    throw Error('arguments must be numbers');
+  }
+
+  const params = {
+    heightInCm: Number(relevantArgs[0]),
+    weightInKg: Number(relevantArgs[1]),
+  };
+
+  if (params.heightInCm <= 0 || params.weightInKg <= 0) {
+    throw Error('arguments must be greater than 0');
+  }
+
+  return params;
+};
+
+const isNumber = (value: any): boolean => !isNaN(Number(value));
+
+try {
+  const params = parseArguments(process.argv);
+  const bmi = calculateBmi(params);
+  console.log(bmi);
+} catch (error) {
+  let message = 'something went wrong';
+  if (error instanceof Error) {
+    message += `: ${error.message}`;
+  }
+  console.log(message);
+}
