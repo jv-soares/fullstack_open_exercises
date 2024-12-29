f5a3c3436f485b917e49b5d7728c9e56de593c63
João Vitor <j.soares@aparkado.de>
2024-12-29 14:23:42 -0300

    display diary entries in a table

--- part9/flight_diary/frontend/src/App.tsx
+++ part9/flight_diary/frontend/src/App.tsx
@@ -1,4 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const App = (): React.Component => {};
+interface DiaryEntry {
+  id: string;
+  date: string;
+  weather: string;
+  visibility: string;
+}
+
+const App = () => {
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/diaries')
+      .then((e) => setDiaries(e.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Flight diaries</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Weather</th>
+            <th>Visibility</th>
+          </tr>
+        </thead>
+        <tbody>
+          {diaries.map((e) => (
+            <tr key={e.id}>
+              <td>{e.date}</td>
+              <td>{e.weather}</td>
+              <td>{e.visibility}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default App;
