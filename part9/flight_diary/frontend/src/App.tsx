5cc3ef36c70893b41c049ce7e7147027cbfbc189
João Vitor <j.soares@aparkado.de>
2024-12-30 20:15:37 -0300

    display errors from backend

--- part9/flight_diary/frontend/src/App.tsx
+++ part9/flight_diary/frontend/src/App.tsx
@@ -2,25 +2,36 @@ import { useEffect, useState } from 'react';
 import { DiaryEntry, NewDiaryEntry } from '../../core/types';
 import DiaryEntryForm from './components/DiaryEntryForm';
 import DiaryTable from './components/DiaryTable';
+import ErrorMessage from './components/ErrorMessage';
 import diaryService from './services/diaryService';
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     diaryService.getAll().then(setDiaries);
   }, []);
 
   const addEntry = (newEntry: NewDiaryEntry) => {
-    diaryService.create(newEntry).then((e) => {
-      const newDiaries = diaries.concat(e);
-      setDiaries(newDiaries);
-    });
+    diaryService
+      .create(newEntry)
+      .then((addedEntry) => {
+        const newDiaries = diaries.concat(addedEntry);
+        setDiaries(newDiaries);
+      })
+      .catch((error) => showErrorMessage(error.response.data));
+  };
+
+  const showErrorMessage = (message: string) => {
+    setErrorMessage(message);
+    setTimeout(() => setErrorMessage(null), 3000);
   };
 
   return (
     <div>
       <h1>Flight diaries</h1>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
       <DiaryEntryForm onSubmitted={addEntry} />
       <DiaryTable diaries={diaries} />
     </div>
---
+++ part9/flight_diary/frontend/src/components/ErrorMessage.tsx
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ message }: { message: string }) => {
+  return <p style={{ color: 'red' }}>{message}</p>;
+};
+
+export default ErrorMessage;
