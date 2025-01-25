67e92d0769d401fb093744b41d351dc1c951a678
João Vitor <j.soares@aparkado.de>
2025-01-25 15:42:08 -0300

    add genre filter to book list

--- part8/small_library/frontend/src/components/Books.jsx
+++ part8/small_library/frontend/src/components/Books.jsx
@@ -1,8 +1,22 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS);
+  const [allGenres, setAllGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const result = useQuery(ALL_BOOKS, {
+    onCompleted: (data) => {
+      const genres = new Set();
+      for (const book of data.allBooks) {
+        for (const genre of book.genres) {
+          genres.add(genre);
+        }
+      }
+      setAllGenres([...genres]);
+    },
+  });
 
   if (!props.show) {
     return null;
@@ -14,10 +28,22 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  const byGenre = (book) => {
+    if (selectedGenres.length === 0) {
+      return book;
+    } else {
+      return book.genres.some((genre) => selectedGenres.includes(genre));
+    }
+  };
+
   return (
     <div>
       <h2>books</h2>
-
+      <GenreFilter
+        allGenres={allGenres}
+        selectedGenres={selectedGenres}
+        onChange={(genres) => setSelectedGenres(genres)}
+      />
       <table>
         <tbody>
           <tr>
@@ -25,7 +51,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((book) => (
+          {books.filter(byGenre).map((book) => (
             <tr key={book.id}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
@@ -34,6 +60,53 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+    </div>
+  );
+};
+
+const GenreFilter = ({ allGenres, selectedGenres, onChange }) => {
+  const changeFilter = (event) => {
+    let newGenres = [];
+    const genre = event.target.value;
+    if (event.target.checked) {
+      newGenres = [...selectedGenres, genre];
+    } else {
+      newGenres = selectedGenres.filter(
+        (selectedGenre) => selectedGenre !== genre
+      );
+    }
+    onChange(newGenres);
+  };
+
+  const selectOrDeselectAll = () => {
+    if (selectedGenres.length === allGenres.length) {
+      onChange([]);
+    } else {
+      onChange(allGenres);
+    }
+  };
+
+  return (
+    <div>
+      {allGenres.map((genre) => (
+        <span key={genre} style={{ marginRight: 10 }}>
+          <input
+            type="checkbox"
+            value={genre}
+            checked={selectedGenres.includes(genre)}
+            onChange={changeFilter}
+          />
+          <label>{genre}</label>
+        </span>
+      ))}
+      <div>
+        <input
+          type="checkbox"
+          checked={selectedGenres.length === allGenres.length}
+          onChange={selectOrDeselectAll}
+        />
+        <label>select/deselect all</label>
+      </div>
     </div>
   );
 };
