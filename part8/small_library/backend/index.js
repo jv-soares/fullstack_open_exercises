3e3a39a49db5ec3814d14b2000e1ba225a742af0
João Vitor <j.soares@aparkado.de>
2025-02-02 17:55:44 +0100

    switch backend to use the expressMiddleware function

--- part8/small_library/backend/index.js
+++ part8/small_library/backend/index.js
@@ -1,13 +1,18 @@
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-
-const User = require('./models/User');
-
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-
+const { expressMiddleware } = require('@apollo/server/express4');
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const jwt = require('jsonwebtoken');
+
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
 require('dotenv').config();
 
@@ -17,21 +22,41 @@ mongoose
   .then(() => console.log('connected to database'))
   .catch((error) => console.log(`error connecting to database: ${error}`));
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7),
-        process.env.JWT_SECRET
-      );
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.toLowerCase().startsWith('bearer ')) {
+          const decodedToken = jwt.verify(
+            auth.substring(7),
+            process.env.JWT_SECRET
+          );
+          const currentUser = await User.findById(decodedToken.id);
+          return { currentUser };
+        }
+      },
+    })
+  );
+
+  const port = 4000;
+  httpServer.listen(port, () => {
+    console.log(`Server ready at http://localhost:${port}`);
+  });
+};
+
+start();
